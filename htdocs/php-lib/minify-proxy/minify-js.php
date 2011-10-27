ad43f857465ec91234fdc7bba6cfb014dcc33b6d
Daiki UEDA <daikiueda.1979@gmail.com>
2011-10-27 17:24:31 +0900

    キャッシュ用ディレクトリ内でディレクトリ構造を持てるよう調整。ただし、ディレクトリは事前に設置する必要アリ。

--- htdocs/php-lib/minify-proxy/minify-js.php
+++ htdocs/php-lib/minify-proxy/minify-js.php
@@ -4,11 +4,12 @@
  * minify js proxy
  * JSファイルの圧縮
  *
- * このPHPプログラムは、URLリライト経由で呼び出されることを前提としています。
- * リライト前のURLから対象のJSファイルを決定し、圧縮処理を実行します。
+ * このPHPプログラムは、サーバ内のリダイレクト処理を経由して呼び出されることを
+ * 前提としています。
+ * リダイレクト前のURLから対象のJSファイルを決定し、圧縮処理を実行します。
  *
  * @author Daiki UEDA
- * @version 1.0
+ * @version 1.0.2
  */
 if( $_SERVER["SCRIPT_NAME"] === $_SERVER["REQUEST_URI"] ){
 	return;
@@ -16,6 +17,9 @@ if( $_SERVER["SCRIPT_NAME"] === $_SERVER["REQUEST_URI"] ){
 
 require_once 'lib/JSMinProxy.php';
 
+preg_match( "/^(.+\/)([^\/]+)$/", $_SERVER["REQUEST_URI"], $matches );
+$sub_directory = preg_replace( "/^\/js\//", "", $matches[1] );
+
 $js_min_proxy = new JSMinProxy( $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"] );
-$js_min_proxy->setCacheDir( $_SERVER["DOCUMENT_ROOT"] . '/js/build' );
+$js_min_proxy->setCacheDir( $_SERVER["DOCUMENT_ROOT"] . '/js/build/' . $sub_directory );
 $js_min_proxy->serve();
