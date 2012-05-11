3c62bcb2640d46e7c1f19965ff0d0cd00face19b
Daiki UEDA <daikiueda.1979@gmail.com>
2012-05-11 17:22:29 +0900

    リクエストされたファイルが存在しない場合の処理を細やかにしてみた。

--- htdocs/php-lib/minify-proxy/minify-js.php
+++ htdocs/php-lib/minify-proxy/minify-js.php
@@ -9,7 +9,7 @@
  * リダイレクト前のURLから対象のJSファイルを決定し、圧縮処理を実行します。
  *
  * @author Daiki UEDA
- * @version 1.0.2
+ * @version 1.0.3
  */
 if( $_SERVER["SCRIPT_NAME"] === $_SERVER["REQUEST_URI"] ){
 	return;
@@ -19,9 +19,23 @@ require_once 'lib/JSMinProxy.php';
 
 $req_uri = preg_replace( "/\?.*$/", "", $_SERVER["REQUEST_URI"] );
 
-preg_match( "/^(.+\/)([^\/]+)$/", $req_uri, $matches );
-$sub_directory = preg_replace( "/^\/js\//", "", $matches[1] );
+$path_source_js = $req_uri;
 
-$js_min_proxy = new JSMinProxy( $_SERVER["DOCUMENT_ROOT"] . $req_uri );
-$js_min_proxy->setCacheDir( $_SERVER["DOCUMENT_ROOT"] . '/js/build/' . $sub_directory );
-$js_min_proxy->serve();
+
+if( is_file( $_SERVER["DOCUMENT_ROOT"] . $path_source_js ) ){
+	preg_match( "/^(.+\/)([^\/]+)$/", $req_uri, $matches );
+	$sub_directory = $matches[1];
+	
+	$js_min_proxy = new JSMinProxy( $_SERVER["DOCUMENT_ROOT"] . $path_source_js );
+	$js_min_proxy->setCacheDir( $_SERVER["DOCUMENT_ROOT"] . $sub_directory . 'build' );
+	$js_min_proxy->serve();
+}
+else {
+	if( file_exists( $_SERVER["DOCUMENT_ROOT"] . $req_uri ) ){
+		header('Content-type: text/javascript');
+		readfile( $_SERVER["DOCUMENT_ROOT"] . $req_uri );
+	}
+	else {
+		header( "HTTP/1.0 404 Not Found" );
+	}
+}
--- htdocs/php-lib/minify-proxy/minify-less.php
+++ htdocs/php-lib/minify-proxy/minify-less.php
@@ -9,7 +9,7 @@
  * リダイレクト前のURLから対象のlessファイルを決定し、パース・出力処理を実行します。
  *
  * @author Daiki UEDA
- * @version 1.0.0
+ * @version 1.0.1
  */
 if( $_SERVER["SCRIPT_NAME"] === $_SERVER["REQUEST_URI"] ){
 	return;
@@ -19,11 +19,23 @@ require_once 'lib/lessphpProxy.php';
 
 $req_uri = preg_replace( "/\?.*$/", "", $_SERVER["REQUEST_URI"] );
 
-preg_match( "/^(.+\/)([^\/]+)$/", $req_uri, $matches );
-$sub_directory = preg_replace( "/^\/common\/css\//", "", $matches[1] );
-
 $path_source_less = preg_replace( '/\.css$/', '.less', $req_uri );
 
-$lessphp_proxy = new lessphpProxy( $_SERVER["DOCUMENT_ROOT"] . $path_source_less );
-$lessphp_proxy->setCacheDir( $_SERVER["DOCUMENT_ROOT"] . '/common/css/build/' . $sub_directory );
-$lessphp_proxy->serve();
+
+if( is_file( $_SERVER["DOCUMENT_ROOT"] . $path_source_less ) ){
+	preg_match( "/^(.+\/)([^\/]+)$/", $req_uri, $matches );
+	$sub_directory = $matches[1];
+
+	$lessphp_proxy = new lessphpProxy( $_SERVER["DOCUMENT_ROOT"] . $path_source_less );
+	$lessphp_proxy->setCacheDir( $_SERVER["DOCUMENT_ROOT"] . '/' . $sub_directory );
+	$lessphp_proxy->serve();
+}
+else {
+	if( file_exists( $_SERVER["DOCUMENT_ROOT"] . $req_uri ) ){
+		header('Content-type: text/css');
+		readfile( $_SERVER["DOCUMENT_ROOT"] . $req_uri );
+	}
+	else {
+		header( "HTTP/1.0 404 Not Found" );
+	}
+}
